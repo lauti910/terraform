cc1e89089e27acaa26fc9475448207c66fc52f8a
lauti910 <lauti910@gmail.com>
2021-12-15 14:53:50 -0800

    Agrego una virtual network

--- main.tf
+++ main.tf
@@ -25,7 +25,10 @@ resource "azurerm_data_factory" "rg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_resource_group" "probando" {
-  name     = "Terraform"
-  location = "eastus2"
+resource "azurerm_virtual_network" "rg" {
+  name                = "vn-ergo-terraform"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
