30886a4c2cafea5d0d68c0debdc2fca09c43e5ca
lauti910 <lauti910@gmail.com>
2021-12-15 11:54:46 -0800

    tftate

--- main.tf
+++ main.tf
@@ -24,32 +24,3 @@ resource "azurerm_data_factory" "rg" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-
-resource "azurerm_resource_group" "test" {
-  name     = "Terraform"
-  location = "eastus2"
-}
-
-resource "azurerm_data_factory" "test" {
-  name                = "df-terraform-test"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-}
-
-resource "azurerm_virtual_network" "test" {
-  name                = "df-ergo-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-}
-
-resource "azurerm_databricks_workspace" "test" {
-  name                = "df-databricks-test"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  sku                 = "standard"
-
-  tags = {
-    Environment = "Production"
-  }
-}
