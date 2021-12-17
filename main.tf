84f03d5655f4379e5dfe32f0a4ccafe19971b125
lauti910 <lauti910@gmail.com>
2021-12-17 10:32:22 -0800

    nuevo rg y gitignore

--- main.tf
+++ main.tf
@@ -18,6 +18,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "SAESA"
   location = "eastus2"
 }
+resource "azurerm_resource_group" "rg" {
+  name     = "ERGO"
+  location = "eastus2"
+}
 
 resource "azurerm_data_factory" "rg" {
   name                = "df-ergo-dev"
@@ -25,31 +29,6 @@ resource "azurerm_data_factory" "rg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_virtual_network" "rg" {
-  name                = "vn-ergo-terraform"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-}
-
-resource "azurerm_data_factory" "rg2" {
-  name                = "df-ergo-dev2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-resource "azurerm_data_factory" "rg3" {
-  name                = "df-ergo-dev3"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-resource "azurerm_data_factory" "rg4" {
-  name                = "df-ergo-dev4"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-resource "azurerm_data_factory" "rg5" {
-  name                = "df-ergo-dev5"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+resource "azurerm_resource_group" "dev" {
+  name     = "SAESA-Dev"
+  location = "eastus2"
