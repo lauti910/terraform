01fce38f07a5e842047f72a849962297cf7eb637
lauti910 <lauti910@gmail.com>
2021-12-17 10:05:00 -0800

    probando123

--- main.tf
+++ main.tf
@@ -32,3 +32,24 @@ resource "azurerm_virtual_network" "rg" {
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
+
+resource "azurerm_data_factory" "rg2" {
+  name                = "df-ergo-dev2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_data_factory" "rg3" {
+  name                = "df-ergo-dev3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_data_factory" "rg4" {
+  name                = "df-ergo-dev4"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_data_factory" "rg5" {
+  name                = "df-ergo-dev5"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
