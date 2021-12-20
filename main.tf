8c8eba490ac7ea69c9cd799c116d83b36eb78acf
lauti910 <lauti910@gmail.com>
2021-12-20 06:53:28 -0800

    Creo todo el grupo de reursos de 0

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 
 variable "resource_group_name" {
   type = string
-  default = "SAESA-dev"
+  default = "SAESA"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -25,7 +25,53 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_data_factory" "rg" {
-  name                = "df-ergo"
+  name                = "df-ergo-prod"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_data_factory" "dev" {
+  name                = "df-ergo-dev-1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_data_factory" "test" {
+  name                = "df-ergo-dev-2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_storage_account" "rg" {
+  name                     = "pruebasaesa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_databricks_workspace" "rg" {
+  name                = "databricks-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_databricks_workspace" "dev" {
+  name                = "databricks-dev"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+
+resource "azurerm_logic_app_workflow" "rg" {
+  name                = "logic_app_ergo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
